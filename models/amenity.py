3d6168d726969b025b03718725a5f778dcd0b848
Yousef Saeed <yousafesaeed@gmail.com>
2023-08-24 00:22:01 +0300

    Remove unused imports

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
 
 
--- models/place.py
+++ models/place.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel, Base
+from os import environ
 from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
-from os import environ
+from models.base_model import BaseModel, Base
 
 place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id',
