fcff3ca5bd83c7f72c72d3860dcc863e39101ef0
Yousef Saeed <yousafesaeed@gmail.com>
2023-08-24 00:17:46 +0300

    Updated amenity, city, review models

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Amenity class that represents new amenities"""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
