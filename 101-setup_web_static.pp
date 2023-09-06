ac31a1376180324607510d4a0cec42708531b149
Yousef Saeed <yousafesaeed@gmail.com>
2023-09-06 20:28:45 +0300

    Changed approach

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,41 +1,11 @@
 # Sets up the web servers for the deployment of web_static using puppet
 
-exec {'/usr/bin/env apt-get update':}
-->package {'nginx':
-ensure => installed,
-}
-
-->file { [ '/data/',
-  '/data/web_static/',
-  '/data/web_static/releases/',
-  '/data/web_static/releases/test/',
-  '/data/web_static/shared/', ]:
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-}
-
-->file {'/data/web_static/releases/test/index.html':
-  ensure  => present,
-  content => "Nginx Server",
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-}
-
-->exec {'/usr/bin/env ln -sf /data/web_static/releases/test/ /data/web_static/current':}
-->exec {'/usr/bin/env chown -R ubuntu:ubuntu /data/':}
-
-->file_line {'add protocol':
-ensure => present,
-path   => '/etc/nginx/sites-available/default',
-after  => 'listen 80 default_server;',
-line   => 'location /hbnb_static/ { alias /data/web_static/current/; autoindex off;}',
-require => Package['nginx'],
-}
-
-->service { 'nginx':
-ensure  => running,
-require => Package['nginx'],
-}
-
-->exec {'/usr/bin/env service nginx restart':}
+exec { '/usr/bin/env apt-get -y update' : }
+-> exec { '/usr/bin/env apt-get -y install nginx' : }
+-> exec { '/usr/bin/env sudo mkdir -p /data/web_static/releases/test/' : }
+-> exec { '/usr/bin/env sudo mkdir -p /data/web_static/shared/' : }
+-> exec { '/usr/bin/env echo "Nginx Server" | sudo tee /data/web_static/releases/test/index.html' : }
+-> exec { '/usr/bin/env sudo ln -sf /data/web_static/releases/test/ /data/web_static/current' : }
+-> exec { '/usr/bin/env sudo chown -R ubuntu:ubuntu /data/' : }
+-> exec { '/usr/bin/env sudo sed -i "/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; autoindex off;}" /etc/nginx/sites-available/default' : }
+-> exec { '/usr/bin/env sudo service nginx restart' : }
