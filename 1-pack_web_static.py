4b2c6bf991e3b2883fa87619012ce7ba91c90aa8
Yousef Saeed <yousafesaeed@gmail.com>
2023-09-06 20:19:44 +0300

    Change datetime format

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,14 +9,8 @@ def do_pack():
     """Generates a .tgz archive from the web_static directory"""
 
     local("mkdir -p versions")
-    now = datetime.today()
     try:
-        file_name = "web_static_{}{}{}{}{}{}.tgz".format(now.year,
-                                                         now.month,
-                                                         now.day,
-                                                         now.hour,
-                                                         now.minute,
-                                                         now.second)
+        file_name = f"web_static_{datetime.now().strftime('%Y%m%d%H%M%S')}.tgz"
         local(f"tar -cvzf versions/{file_name} web_static")
         return file_name
     except Exception:
