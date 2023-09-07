efd70424f1c0e73653db0a302badfe6c4aabe4e2
Yousef Saeed <yousafesaeed@gmail.com>
2023-09-08 00:18:25 +0300

    Revert back to old version

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,22 +1,39 @@
 #!/usr/bin/python3
 """Cleans outdated archives"""
 
-from fabric.api import *
-from os import listdir
+from fabric.api import local, sudo
+from io import StringIO
 
 
 def do_clean(number=0):
     """Cleans outdated archives"""
 
-    number = 1 if int(number) == 0 else int(number)
-    archives = sorted(listdir("versions"))
-    [archives.pop() for _ in range(number)]
+    file_handler = StringIO()
+    versions = []
+    number = int(number)
 
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+    if number == 0:
+        number = 1
 
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for _ in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+    reverse_ls = "ls -ltr versions | rev | cut -d ' ' -f1 | rev"
+
+    value = local(reverse_ls, capture=True)
+    for line in value.splitlines():
+        versions.append(line)
+
+    versions.pop(0)
+    for i in range(len(versions) - number):
+        local("rm -rf versions/{}".format(versions[i]))
+
+    code = "ls -ltr /data/web_static/releases | rev | cut -d ' ' -f1 | rev"
+    fk_value = sudo(code, stdout=file_handler)
+
+    file_handler.seek(0)
+    fk_names = []
+    for line in file_handler.readlines():
+        data = line.split()[-1]
+        if data.startswith("web_static"):
+            fk_names.append(data)
+
+    for i in range(len(fk_names) - number):
+        sudo("rm -rf /data/web_static/releases/{}".format(fk_names[i]))
