c2e443dff81007df1d96477c08a13c01ec03fa9f
Yousef Saeed <yousafesaeed@gmail.com>
2023-09-21 21:01:17 +0300

    Display states & cities

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Starts a Flask web application listening on 0.0.0.0, port 5000"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/states_list")
+def states_list():
+    """/states_list: display a HTML page: (inside the tag 'BODY')"""
+    states = sorted(list(storage.all("State").values()), key=lambda s: s.name)
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Starts a Flask web application listening on 0.0.0.0, port 5000"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states_list():
+    """/states: display a HTML page: (inside the tag BODY)
+    /states/<id>: display a HTML page: (inside the tag BODY)"""
+    return render_template("8-cities_by_states.html", sts=storage.all("State"))
+
+
+@app.teardown_appcontext
+def use_teardown(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
