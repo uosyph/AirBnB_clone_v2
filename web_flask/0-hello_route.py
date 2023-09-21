5d4fde5a4f0bcdba58781556008a373bd3cf1efd
Yousef Saeed <yousafesaeed@gmail.com>
2023-09-21 16:51:49 +0300

    Flask routes

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Starts a Flask web application listening on 0.0.0.0, port 5000"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def index():
+    """Route index"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Starts a Flask web application listening on 0.0.0.0, port 5000"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def index():
+    """Route index"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """Route index"""
+    return "HBNB"
+
+
+@app.route("/c/<string:text>")
+def ctext(text):
+    """Route index"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python/", defaults={"text": "is cool"})
+@app.route("/python/<string:text>")
+def pythontext(text="is cool"):
+    """Route index"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
